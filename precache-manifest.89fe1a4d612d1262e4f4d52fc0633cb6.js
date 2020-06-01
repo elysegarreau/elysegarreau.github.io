7ee6dba1985bea05a7eb7f66d259a4bd78a22725
Elyse Garreau <egarreau@berkeley.edu>
2020-05-31 18:08:35 -0700

    Updates

---
+++ precache-manifest.89fe1a4d612d1262e4f4d52fc0633cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfc8f80c8f74a224aefa7d349a0fd25",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "78b4dde9adaa3c63a469",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/myapp/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b4dde9adaa3c63a469",
+    "url": "/myapp/static/js/main.0efb3bc4.chunk.js"
+  },
+  {
+    "revision": "8a08ee084d14eb31cf8b",
+    "url": "/myapp/static/js/runtime-main.646e23b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
